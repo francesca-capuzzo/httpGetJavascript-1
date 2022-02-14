464b7e6a72ef7d41f0d80c8948412449d6aba978
LolloMira <lorenzo23082000@gmail.com>
2022-02-14 13:24:36 +0100

    list with some css

--- script.js
+++ script.js
@@ -18,7 +18,55 @@
     // }
  
 
-    fetch("./assets/data.json")
+    // fetch("./assets/data.json")
+    // .then(manageResponse)
+    // .then(onDataReady)
+    // .catch(onError);
+
+    // function manageResponse(response) {
+    //     return response.json();
+    // }
+
+    // function onDataReady(data) {
+    //     for (const user of data) {
+    //         console.log("nome: ", user.username);
+    //     }
+    // }
+
+    // function onError(error) {
+    //    console.log(error);
+    // }
+
+
+    // fetch("./assets/books.json")
+    // .then(manageResponse)
+    // .then(onDataReady)
+    // .catch(onError);
+
+    // function manageResponse(response) {
+    //     return response.json();
+    // }
+
+    
+                       
+   
+    // function onDataReady(books) {
+    //     let par = document.getElementById("book-list");
+    //     for (const book of books) {
+    //         var li = document.createElement('li');
+    //         li.appendChild(document.createTextNode("TITOLO\n"+ book.titolo +"\nGENERE "+ book.genere + "\nANNO " + book.anno));
+    //         par.appendChild(li);
+    //     }
+    // }
+
+    // function onError(error) {
+    //    console.log(error);
+    // }
+
+
+
+
+    fetch("./assets/books.json")
     .then(manageResponse)
     .then(onDataReady)
     .catch(onError);
@@ -27,12 +75,30 @@
         return response.json();
     }
 
-    function onDataReady(data) {
-        for (const user of data) {
-            console.log("nome: ", user.username);
+    
+                       
+   
+    function onDataReady(books) {
+        let par = document.getElementById("book-list");
+        for (const book of books) {
+            var li = document.createElement('li');
+            li.className = "liStyle";
+            let ul2 = document.createElement("ul")
+            let li2 = document.createElement("li");
+            let li3 = document.createElement("li");
+            li.appendChild(document.createTextNode(book.titolo));
+            li2.appendChild(document.createTextNode("GENERE: " + book.genere));
+            li3.appendChild(document.createTextNode("ANNO: " + book.anno));
+        
+            par.appendChild(li);
+            li.appendChild(ul2);
+            ul2.appendChild(li2);
+            ul2.appendChild(li3);
         }
     }
 
     function onError(error) {
        console.log(error);
     }
+
+
