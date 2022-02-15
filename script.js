416f50e8016150776a1be1218b4969654c05ad25
francesca-capuzzo <f.capuzzo9966@gmail.com>
2022-02-15 09:20:30 +0100

    another version in class

--- script.js
+++ script.js
@@ -1,74 +1,7 @@
-    // const xhttp = new XMLHttpRequest();
-
-    // httpRequest.onreadystatechange = gestisciRisposteDelServer;
-
-    // httpRequest.open('GET', 'test.html');
-
-    // httpRequest.send();
-
-
-    // function gestisciRisposteDelServer() {
-    //     console.log("ready state", httpRequest.readyState);
-    //     console.log("status", httpRequest.status );
-    //     console.log("response", httpRequest.responseText);
-    //     if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-    //        const arrayOfUsers = JSON.parse(httpRequest.responseText);
-    //        console.log(arrayOfUsers);;
-    //    }
-    // }
- 
-
-    // fetch("./assets/data.json")
-    // .then(manageResponse)
-    // .then(onDataReady)
-    // .catch(onError);
-
-    // function manageResponse(response) {
-    //     return response.json();
-    // }
-
-    // function onDataReady(data) {
-    //     for (const user of data) {
-    //         console.log("nome: ", user.username);
-    //     }
-    // }
-
-    // function onError(error) {
-    //    console.log(error);
-    // }
-
-
-    // fetch("./assets/books.json")
-    // .then(manageResponse)
-    // .then(onDataReady)
-    // .catch(onError);
-
-    // function manageResponse(response) {
-    //     return response.json();
-    // }
-
-    
-                       
-   
-    // function onDataReady(books) {
-    //     let par = document.getElementById("book-list");
-    //     for (const book of books) {
-    //         var li = document.createElement('li');
-    //         li.appendChild(document.createTextNode("TITOLO\n"+ book.titolo +"\nGENERE "+ book.genere + "\nANNO " + book.anno));
-    //         par.appendChild(li);
-    //     }
-    // }
-
-    // function onError(error) {
-    //    console.log(error);
-    // }
-
-
-
 
     fetch("./assets/books.json")
     .then(manageResponse)
-    .then(onDataReady)
+    .then(onDataReady2)
     .catch(onError);
 
     function manageResponse(response) {
@@ -78,27 +11,58 @@
     
                        
    
-    function onDataReady(books) {
-        let par = document.getElementById("book-list");
-        for (const book of books) {
-            var li = document.createElement('li');
-            li.className = "liStyle";
-            let ul2 = document.createElement("ul")
-            let li2 = document.createElement("li");
-            let li3 = document.createElement("li");
-            li.appendChild(document.createTextNode(book.titolo));
-            li2.appendChild(document.createTextNode("GENERE: " + book.genere));
-            li3.appendChild(document.createTextNode("ANNO: " + book.anno));
+    // function onDataReady(books) {
+    //     let par = document.getElementById("book-list");
+    //     for (const book of books) {
+    //         var li = document.createElement('li');
+    //         li.className = "liStyle";
+    //         let ul2 = document.createElement("ul")
+    //         let li2 = document.createElement("li");
+    //         let li3 = document.createElement("li");
+    //         li.appendChild(document.createTextNode(book.titolo));
+    //         li2.appendChild(document.createTextNode("GENERE: " + book.genere));
+    //         li3.appendChild(document.createTextNode("ANNO: " + book.anno));
         
-            par.appendChild(li);
-            li.appendChild(ul2);
-            ul2.appendChild(li2);
-            ul2.appendChild(li3);
-        }
+    //         par.appendChild(li);
+    //         li.appendChild(ul2);
+    //         ul2.appendChild(li2);
+    //         ul2.appendChild(li3);
+    //     }
+    // }
+
+    // function onError(error) {
+    //    console.log(error);
+    // }
+
+//3 MODI PER METTERE TESTO NELL'HTML
+// li.textContent() 
+// li.innerHTML()
+// li.createTextNode()
+
+function addTextToHtmlElement(htmlElement, text, isNewLine = false, className) {
+    const span = document.createElement('span');
+    span.className += className + " "; // ---->+= aggiunge alla classe una nuova classe + uno spazio così che posso dare più classi allo stesso oggetto
+    const textNode = document.createTextNode(text);
+    span.appendChild(textNode)
+    htmlElement.appendChild(span);
+    if (isNewLine) {
+        const newLine = document.createElement('br');
+        htmlElement.appendChild(newLine)
     }
+}
 
-    function onError(error) {
-       console.log(error);
+function onDataReady2(data) {
+    const list = document.getElementById("book-list");
+    for (const book of data) {
+        const listElement = document.createElement("li");
+        listElement.className += "book-card" + " ";
+        addTextToHtmlElement(listElement, book.titolo, true, "bold large-font");
+        addTextToHtmlElement(listElement, book.genere, true, "bold");
+        addTextToHtmlElement(listElement, book.anno);
+        list.appendChild(listElement);
     }
+}
 
-
+function onError(error) {
+    console.log(error);
+}
